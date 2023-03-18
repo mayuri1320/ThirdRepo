1c666b915cc79d90a853af12eb723e366ef360dd
mayuri1320 <mayurichandel105@gmail.com>
2023-03-18 16:26:55 +0530

    Xyz.java Created

---
+++ xyz.java
@@ -0,0 +1,23 @@
+public class Xyz
+{
+int a=2313;
+char b='A';
+float c=0.005f;
+double d=0.555;
+long e=13546541l;
+//boolean f=True;
+String g="anything";
+public static void main(String args[])
+{
+System.out.println("Main Start");
+D obj=new D();
+System.out.println(obj.a);
+System.out.println(obj.b);
+System.out.println(obj.c);
+System.out.println(obj.d);
+System.out.println(obj.e);
+System.out.println(obj.g);
+System.out.println("Main End");
+
+}
+}
